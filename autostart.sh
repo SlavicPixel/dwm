7685f98c2e7ecb286a709d595cbd6f89c924d831
pixel <domi9132@gmail.com>
2022-09-26 20:12:07 +0200

    Applied status2d with systray patch

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 nitrogen --restore &
-numlockx on
+numlockx on &
+dwmblocks &
+volumeicon &
+mullvad-vpn &
+cmst &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+/usr/bin/emacs --daemon &
+nvidia_pl &
